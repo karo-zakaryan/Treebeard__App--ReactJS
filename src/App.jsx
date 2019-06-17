97597dcdb03754c3f716a64d6f5b520bdf29f220
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2019-06-17 22:32:53 +0400

    Continue build my tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useDataApi } from "./customHooks/useDataApi";
+
 import Header from "./components/container/Header";
 import Main from "./components/container/Main";
+import Spinner from "./components/presentational/Spinner";
 
-const App = () => (
-  <>
-    <Header />
-    <Main />
-  </>
-);
+const App = () => {
+  const [{ data, isLoading }] = useDataApi();
+
+  return (
+    <>
+      <Header />
+      {!isLoading && data.length ? <Main data={data} /> : <Spinner />}
+    </>
+  );
+};
 
 export default App;
---
+++ src/constants/fakeData.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    type: "folder",
+    name: "Importantworkstuff",
+    files: [
+      {
+        type: "file",
+        name: "quarterly-results.xlsx"
+      }
+    ]
+  },
+  {
+    type: "folder",
+    name: "AnimalGIFs",
+    files: [
+      {
+        type: "folder",
+        name: "Dogs",
+        files: [
+          {
+            type: "file",
+            name: "treadmill.gif"
+          },
+          {
+            type: "file",
+            name: "rope-jumping.gif"
+          }
+        ]
+      },
+      {
+        type: "folder",
+        name: "Goats",
+        files: [
+          {
+            type: "file",
+            name: "parkour.gif"
+          },
+          {
+            type: "file",
+            name: "rampage.gif"
+          }
+        ]
+      },
+      {
+        type: "file",
+        name: "cat-roomba.gif"
+      },
+      {
+        type: "file",
+        name: "duck-shuffle.gif"
+      },
+      {
+        type: "file",
+        name: "monkey-on-a-pig.gif"
+      }
+    ]
+  },
+  {
+    type: "file",
+    name: "TODO.md"
+  }
+];
