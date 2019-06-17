6c64ca830cc4d517fd083ac2b4e50a99d1573a3d
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2019-06-17 21:29:34 +0400

    Initital commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { register } from "./serviceWorker";
+
+import App from "./App.jsx";
+
+const wrapper = document.getElementById("root");
+
+register();
+ReactDOM.render(<App />, wrapper);
